8b6395d05ce780bda0954aa5f1bf44a4d512ff68
Mark Meijer <mjcmeije@avans.nl>
2017-05-27 15:14:46 +0200

    Added main activity

--- ATPS_Assistent/app/src/main/java/nl/avans/mm/atps_assistent/MainActivity.java
+++ ATPS_Assistent/app/src/main/java/nl/avans/mm/atps_assistent/MainActivity.java
@@ -2,6 +2,7 @@ package nl.avans.mm.atps_assistent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 	
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		WebView webView = (WebView)findViewById(R.id.webView);
+//you can load an html code
+		//webView.loadData("yourCode Html to load on the webView " , "text/html" , "utf-8");
+// you can load an URL
+		webView.loadUrl("http://188.205.24.245/pctwa/login.aspx");
 	}
 }
